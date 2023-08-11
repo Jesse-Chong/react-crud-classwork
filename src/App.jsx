1a7b5ef46ca77811324bcbd80723840b10caf875
Jesse Chong <jessechong@pursuit.org>
2023-08-11 12:29:48 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { BrowserRouter as Router} from 'react-router-dom'
+import './App.css'
+
+function App() {
+console.log("Our Environment Variables: ", import.meta.env.VITE_10_1_VAR)
+  return (
+    <>
+  <Router>
+    <h1>ScreenViews</h1>
+
+    <h2>-TEST-{import.meta.env.VITE_10_1_VAR}</h2>
+</Router>
+    </>
+  )
+}
+
+export default App
